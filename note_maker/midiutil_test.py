7321e9d226f360acbf051e65f309e94a56c31458
Adam <57573881+AdamCarter11@users.noreply.github.com>
2021-11-15 22:10:56 -0800

    Added notes about fitness

--- note_maker/midiutil_test.py
+++ note_maker/midiutil_test.py
@@ -1,5 +1,5 @@
 from midiutil import MIDIFile
-
+import random
 degrees  = [60, 62, 64, 65, 67, 69, 71, 72] # MIDI note number
 track    = 0
 channel  = 0
@@ -13,8 +13,31 @@ MyMIDI = MIDIFile(1) # One track, defaults to format 1 (tempo track
 MyMIDI.addTempo(track,time, tempo)
 
 for pitch in degrees:
-    MyMIDI.addNote(track, channel, pitch, time, duration, volume)
+    #MyMIDI.addNote(track, channel, pitch, time, duration, volume)
+    MyMIDI.addNote(track, channel, degrees[random.randint(0,len(degrees)-1)], time, duration, volume)
     time = time + 1
 
 with open("major-scale.mid", "wb") as output_file:
     MyMIDI.writeFile(output_file)
+
+'''
+Fitness stuff:
+    generally only use 8 out the 12 notes
+    chords are made by skipping notes
+    theres 3 notes that you play almost all the time (called chords), note 1, note 3, and note 5
+        Notes 2,4,6 sounds really good together (8 is an octive higher than 1, higher pitch)
+        3 5 7
+        4 6 8
+        G B D (in note form) this would be a good closer (another would be F A C)
+        basically, skipping notes sounds good
+    C-C is called an octive (8 notes in total)
+    2 4 6 goes really nice into G B C
+    sometimes we want G chord to go to A chord (less frequently)
+    chords that overlap sounds good together
+    major cadency sounds good 5-1 or 4-1
+    6 and 3 are substitution chords for 1 (but don't start or end with them)
+    try to not use the B chord (if the chord is B D F, pick a different chord) it sounds cool, but needs lots of rules around it
+    chords 4 and 5 can substitute for each other
+    chord 2 can sometimes be used instead of chord 4 or 5 (rarely)
+    Never want to have more than 4 notes happen at one time (snare drum is included in that 4)
+'''
